015343850fca877637c36e90f808ed5a379fb9c9
TomasGiacoboni19 <tgiacoboni@frba.utn.edu.ar>
2025-07-17 02:32:54 +0000

    Agrego fix marcos libres

--- memoria/src/marcos_libres.c
+++ memoria/src/marcos_libres.c
@@ -7,6 +7,8 @@
 t_bitarray* bitmap_marcos;
 int cantidad_total_marcos;
 
+
+// Crea un bitmap con todos los marcos en 0 (libres)
 void inicializar_bitmap(int cantidad_frames) {
     cantidad_total_marcos = cantidad_frames;
     int bytes_necesarios = (cantidad_frames + 7) / 8;
@@ -14,6 +16,7 @@ void inicializar_bitmap(int cantidad_frames) {
     bitmap_marcos = bitarray_create_with_mode(buffer, bytes_necesarios, LSB_FIRST);
 }
 
+// Recorre el bitmap buscando al menos un marco libre
 bool hay_marcos_libres() {
     for (int i = 0; i < cantidad_total_marcos; i++) {
         if (!bitarray_test_bit(bitmap_marcos, i)) {
@@ -23,6 +26,8 @@ bool hay_marcos_libres() {
     return false;
 }
 
+
+// Obtiene el marco libre para ocuparlo y después devolverlo
 int obtener_marco_libre() {
     for (int i = 0; i < cantidad_total_marcos; i++) {
         if (!bitarray_test_bit(bitmap_marcos, i)) {
@@ -33,12 +38,14 @@ int obtener_marco_libre() {
     return -1; // No hay marcos libres
 }
 
+// Marca como libre un marco usado
 void liberar_marco(int marco) {
     if (marco >= 0 && marco < cantidad_total_marcos) {
         bitarray_clean_bit(bitmap_marcos, marco);
     }
 }
 
+// Libera de la memoria el bitmap
 void destruir_bitmap() {
     free(bitmap_marcos->bitarray);
     bitarray_destroy(bitmap_marcos);
